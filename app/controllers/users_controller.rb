f5c7a26db1fe4b29f606f69a14f0d61b90a48be4
smcommits <sm.anas@outlook.com>
2020-12-15 19:01:00 +0530

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def new 
+  def new
     @user = User.new
   end
 
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-   #  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update 
+  def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
       redirect_to @user
     else
