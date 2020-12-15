770f73ad5e546c00f91a4752eb75a9773803c1dd
smcommits <sm.anas@outlook.com>
2020-12-15 18:22:17 +0530

    Add functionality to edit user details
    Added functionality to edit user detials by implementing edit and update
    actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
    #  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new
     end
@@ -17,6 +21,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update 
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
